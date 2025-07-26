058808fbda2fb5f45665f38f129ee0661d02d8b6
HishamFawwzy0 <hishamfawzy16@gmail.com>
2025-07-26 06:23:06 +0300

    vercel fix v2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,12 @@
 import { Routes } from '@angular/router';
 import { NotFound } from './pages/not-found/not-found';
 import { Home } from './pages/home/home';
+import { ProductDetails } from './pages/product-details/product-details';
 
 export const routes: Routes = [
     // Define your application routes here
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', loadComponent: () => import('./pages/home/home').then(m => m.Home) },
-    { path: 'product-details/:id', loadComponent: () => import('./pages/product-details/product-details').then(m => m.ProductDetails) },
+    { path: 'product-details/:id', component:ProductDetails },
     { path: '**', loadComponent: () => import('./pages/not-found/not-found').then(m => m.NotFound) }
 ];
